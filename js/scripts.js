9c71337f25a62748e7ecc80728618dcbbdf34529
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-12 16:04:51 -0700

    add add/subtract/divide/multiply feature to html including forms and jquery logic
    CO-AUTHORED by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,50 @@
+// Business logic:
+
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is user interface logic:
+
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("form#add").submit(function(event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val())
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val())
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val())
+    const result = divide(number1, number2);
+    $("#output").text(result);
   });
 });
